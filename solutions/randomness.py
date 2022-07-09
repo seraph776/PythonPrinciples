49156d11f23972a806849ef0ed2b3f7ddd59f485
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-07-09 14:31:10 -0400

    Update randomness.py

--- solutions/randomness.py
+++ solutions/randomness.py
@@ -3,7 +3,7 @@
 created: 2022-07-09 12:30:45
 @author: seraph★1001100
 contact: admin@codecrypt76.com
-project: Python Principles; Middle Letter
+project: Python Principles; Randomness
 metadoc: Define a function, random_number, that takes no parameters. The function must generate a random integer
          between 1 and 100, both inclusive, and return it. Calling the function multiple times should (usually)
          return different numbers.
