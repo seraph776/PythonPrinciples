77c24a09d7dc6249386298cd56a670a2d313e21f
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-07-09 14:43:40 -0400

    Create divisible_by_3.py

---
+++ solutions/divisible_by_3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+created: 2022-07-09 12:30:45
+@author: seraph★1001100
+contact: admin@codecrypt76.com
+project: Python Principles; Anagrams
+metadoc: Define a function named div_3 that returns True if its single integer parameter is
+         divisible by 3 and False otherwise.
+license: None
+"""
+
+
+def div_3(n):
+    return n % 3 == 0
