e44283ac07b3fd5419cb368474d5f249951cd716
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-07-09 14:40:48 -0400

    Update counting_syllables.py

--- solutions/counting_syllables.py
+++ solutions/counting_syllables.py
@@ -3,7 +3,7 @@
 created: 2022-07-09 12:30:45
 @author: seraph★1001100
 contact: admin@codecrypt76.com
-project: Python Principles; Add/Remove Dots
+project: Python Principles; Counting Syllables
 metadoc: Define a function named count that takes a single parameter. The parameter is a string.
          The string will contain a single word divided into syllables by hyphens
 license: None
