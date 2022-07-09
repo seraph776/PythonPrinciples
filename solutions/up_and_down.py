7d0ff374e02930a7ce5298797b9a05f360da2724
Seraph★776 <72005563+seraph776@users.noreply.github.com>
2022-07-09 14:50:45 -0400

    Create up_and_down.py

---
+++ solutions/up_and_down.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+created: 2022-07-09 12:30:45
+@author: seraph★1001100
+contact: admin@codecrypt76.com
+project: Python Principles; Up and Down
+metadoc: Define a function named up_down that takes a single number as its parameter.
+         Your function return a tuple containing two numbers; the first should be one lower than the parameter,
+         and the second should be one higher.
+license: None
+"""
+
+
+def up_down(n):
+    return n - 1, n + 1
